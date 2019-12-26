88ce6e44bdfc1451933c35b3bf3fbf5212017b4d
quocbaodoan <54675143+quocbaodoan@users.noreply.github.com>
2019-12-26 10:24:16 +0700

    Add files via upload

---
+++ RanSanMoi/RanSanMoi/Dragon.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RanSanMoi
+{
+    class Dragon
+    {
+        SolidBrush br;
+        Rectangle[] recdragon;
+        private int x, y, width, height;
+        public Rectangle[] Recdragon
+        {
+            get
+            {
+                return recdragon;
+            }
+        }
+        public Dragon()
+        {
+            br = new SolidBrush(Color.Blue);
+            recdragon = new Rectangle[4];
+            x = 20;
+            y = 20;
+            width = 15;
+            height = 15;
+            for (int i = 0; i < recdragon.Length; i++)
+            {
+                recdragon[i] = new Rectangle(x, y, width, height);
+                x += 10;
+            }
+        }
+        public void DrawDragon(Graphics paper)
+        {
+            foreach (Rectangle item in recdragon)
+            {
+                paper.FillEllipse(br, item);
+            }
+        }
+        public void DrawDragonRun()
+        {
+            for (int i = recdragon.Length - 1; i >= 0; i--)
+            {
+                recdragon[i] = recdragon[i - 1];
+            }
+        }
+        public void DragonDown()
+        {
+            DrawDragonRun();
+            recdragon[0].Y += 15;
+        }
+        public void DragonUp()
+        {
+            DrawDragonRun();
+            recdragon[0].Y -= 15;
+        }
+        public void DragonLeft()
+        {
+            DrawDragonRun();
+            recdragon[0].X -= 15;
+        }
+        public void DragonRight()
+        {
+            DrawDragonRun();
+            recdragon[0].X += 15;
+        }
+    }
+}
---
+++ RanSanMoi/RanSanMoi/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RanSanMoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dragon dragon = new Dragon();
+        Graphics g;
+        bool left = false, right = false, up = false, down = false;
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (left == true)
+            {
+                dragon.DragonLeft();
+            }
+            if(right == true)
+            {
+                dragon.DragonRight();
+            }
+            if (up == true)
+            {
+                dragon.DragonUp();
+            }
+            if (down == true)
+            {
+                dragon.DragonDown();
+            }
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            dragon.DrawDragon(g);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left && right == false)
+            {
+                left = true;
+                right = false;
+                up = false;
+                down = false;
+            }
+            if (e.KeyData == Keys.Right && left == false)
+            {
+                left = false;
+                right = true;
+                up = false;
+                down = false;
+            }
+            if (e.KeyData == Keys.Up && down == false)
+            {
+                left = false;
+                right = false;
+                up = true;
+                down = false;
+            }
+            if (e.KeyData == Keys.Down && up == false)
+            {
+                left = false;
+                right = false;
+                up = false;
+                down = true;
+            }
+        }
+    }
+}
